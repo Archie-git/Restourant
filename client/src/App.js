35be8e2b31bec22b3c835d2f460f77af6274db79
Archie <643995875@qq.com>
2019-12-22 00:17:03 +0800

    引入Antd

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {Button,message} from 'antd';
 
 class App extends React.Component{
+    handleClick=()=>{
+        message.info("kkkkkkkkk")
+    };
     render(){
-        return <div>餐饮服务管理系统</div>
+        return <div>
+            <p>餐饮服务管理系统</p>
+            <Button type="primary" onClick={this.handleClick}>Click me</Button>
+        </div>
     }
 }
 
